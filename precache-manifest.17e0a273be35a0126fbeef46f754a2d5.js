93e70694e8cef096c1b96761cc918c498d3d8822
basel-3mk <baslimadj@gmail.com>
2026-07-30 18:45:52 +0300

    Updates

---
+++ precache-manifest.17e0a273be35a0126fbeef46f754a2d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dc7134cc5ef78b738e38abeeda151c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c3307373b87e2265bf5",
+    "url": "./static/css/main.a5421ce8.chunk.css"
+  },
+  {
+    "revision": "74d8b5c782158c15e4b9",
+    "url": "./static/js/2.6ac9403a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6ac9403a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3307373b87e2265bf5",
+    "url": "./static/js/main.3a2f3ed8.chunk.js"
+  },
+  {
+    "revision": "27dbd2d6ef8732349f8a",
+    "url": "./static/js/runtime-main.2a92e956.js"
+  },
+  {
+    "revision": "a82c4c3bfe6cd8a9d57597071772d0c7",
+    "url": "./static/media/Kanit.a82c4c3b.ttf"
+  },
+  {
+    "revision": "0113e4ab0457064ceff687ef5f2f52c2",
+    "url": "./static/media/codeforces_logo.0113e4ab.png"
+  },
+  {
+    "revision": "89c2c0674bdba3d61daf6b1b1e5e82b5",
+    "url": "./static/media/gh-logo.89c2c067.png"
+  },
+  {
+    "revision": "43e3c67e054561d7687a03b98fcf3170",
+    "url": "./static/media/loop.43e3c67e.svg"
+  }
+]);
